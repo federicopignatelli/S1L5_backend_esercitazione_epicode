26e93a2392413df4235ea6a6ba892bf344224474
federicopignatelli <federicopignatelli@icloud.com>
2023-12-01 14:29:22 +0100

    classe immagine creata

--- src/Entities/Immagine.java
+++ src/Entities/Immagine.java
@@ -1,4 +1,34 @@
 package Entities;
 
-public class Immagine {
+import Interfaces.Luminosità;
+
+public class Immagine extends ElementoMultimediale implements Luminosità {
+    public int luminosita;
+    public Immagine(String titolo, int luminosita) {
+        super(titolo);
+        this.luminosita= luminosita;
+    }
+
+    @Override
+    public void piuLuminosità() {
+        if (luminosita < 10) {
+            luminosita++;
+            System.out.println("luminosità aumentata" + luminosita);
+        } else {
+            System.out.println("luminosità max");
+        }
+    }
+    @Override
+    public void menoLuminosità() {
+        if (luminosita > 0) {
+            luminosita--;
+            System.out.println("luminosità diminuita" + luminosita);
+        } else {
+            System.out.println("luminosità min");
+        }
+    }
+    @Override
+    public String toString() {
+        return "Title: " + getTitle() + ", Luminosità: " + luminosita;
+    }
 }
